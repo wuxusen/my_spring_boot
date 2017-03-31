60272cf05d469a0c1623dabf4c2651bb24079312
wuxusen <wuxusenn@outlook.com>
2017-03-31 16:03:22 +0800

    添加logback

---
+++ src/test/java/com/wxs/LogbackDemo.java
@@ -0,0 +1,15 @@
+package com.wxs;
+  
+import org.slf4j.Logger;  
+import org.slf4j.LoggerFactory;  
+  
+public class LogbackDemo {  
+    private static Logger log = LoggerFactory.getLogger(LogbackDemo.class);  
+    public static void main(String[] args) {  
+        log.trace("======trace");  
+        log.debug("======debug");  
+        log.info("======info");  
+        log.warn("======warn");  
+        log.error("======error");  
+    }  
+}  
